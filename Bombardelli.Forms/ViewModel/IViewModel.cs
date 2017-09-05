23ae86de61e51b1e3eecd53a72b353146be00cb4
JC Bombardelli <jc.bombardelli@live.com>
2017-09-05 00:49:34 -0300

    ViewModel bases added

---
+++ Bombardelli.Forms/ViewModel/IViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Bombardelli.Forms.ViewModel
+{
+    public interface IViewModel : INotifyPropertyChanged
+    {
+        event EventHandler ViewModelLoaded;
+        void OnViewModelLoaded();
+    }
+}
---
+++ Bombardelli.Forms/ViewModel/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace Bombardelli.Forms.ViewModel
+{
+    public abstract class ViewModel : IViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler ViewModelLoaded;
+
+        public static SynchronizationContext UISynchronizationContext { get; set; }
+
+        public virtual void OnViewModelLoaded()
+        {
+            ViewModelLoaded?.Invoke(this, null);
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
