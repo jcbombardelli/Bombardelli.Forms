d881f37dbc096a26942c981cfa8c3f3624358760
JC Bombardelli <jc.bombardelli@live.com>
2017-09-05 01:02:46 -0300

    Color extension added

---
+++ Bombardelli.Forms/Extensions/Colors.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace Bombardelli.Forms.Extensions
+{
+    public static class Colors
+    {
+        public static Color Primary { get; set; }
+        public static Color Accent { get; set; }
+        public static Color FontPrimary { get; set; }
+        public static Color FontAccent { get; set; }
+        public static Color Placeholder { get; set; }
+        public static Color BackgroundColor { get; set; }
+
+    }
+}
